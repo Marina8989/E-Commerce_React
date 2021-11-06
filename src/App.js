d7be53471ead82b2c5478fe4c016d9a4676838bd
Marina8989 <marina.mill209@gmail.com>
2021-11-06 13:52:21 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
+import {  commerce } from './lib/commerce';
 import {Navbar, Products} from './components';
 
 const App = () => {
+    const [products, setProducts] = useState([])
     return(
         <div>
             <Navbar />
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Product from './Product/Product';
 
+import useStyles from './styles'
+
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzjxphP5tzHlG0MnR6oFQlUrdqzcZ0vK__yQ&usqp=CAU'},
     {id: 2, name: 'Macbook', description: 'Apple macbook', price:'$10', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzjxphP5tzHlG0MnR6oFQlUrdqzcZ0vK__yQ&usqp=CAU'}
 ]
 
 const Products = () => {
+  const classes = useStyles();
     return(
-      <main>
+      <main className={classes.content}>
+      <div className={classes.toolbar} />
           <Grid container justify='center' spacing={4}>
             {products.map(product => {
                return(
